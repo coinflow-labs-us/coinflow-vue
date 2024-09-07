452e39cddde68b69d9dbe22e059d2a826930464f
coinflow-labs-us <ben@coinflowlabs.app>
2024-09-07 18:32:26 +0000

    Update from https://github.com/coinflow-labs-us/coinflow/commit/52b3127d2cbc0fc145d5a79303c57160ddda71dc

--- src/testing/MobileWalletButtonTest.vue
+++ src/testing/MobileWalletButtonTest.vue
@@ -1,28 +1,54 @@
 <script setup lang="ts">
-import CoinflowGooglePayButton from "../components/mobile-wallet/CoinflowGooglePayButton.vue";
-import CoinflowApplePayButton from "../components/mobile-wallet/CoinflowApplePayButton.vue";
-import { CoinflowPurchaseProps } from "../lib/common";
-import { Connection, Keypair } from "@solana/web3.js";
+import CoinflowGooglePayButton from '../components/mobile-wallet/CoinflowGooglePayButton.vue';
+import CoinflowApplePayButton from '../components/mobile-wallet/CoinflowApplePayButton.vue';
+import {
+  CoinflowPurchaseProps,
+  CoinflowSolanaPurchaseProps,
+} from '../lib/common';
+import {Connection, Keypair} from '@solana/web3.js';
+
+const wallet: CoinflowSolanaPurchaseProps['wallet'] = {
+  publicKey: Keypair.generate().publicKey,
+  sendTransaction: () => {
+    throw new Error('Not Implemented');
+  },
+  signMessage: () => {
+    throw new Error('Not Implemented');
+  },
+} as CoinflowSolanaPurchaseProps['wallet'];
 
 const args: CoinflowPurchaseProps & {color: 'white' | 'black'} = {
-  env: 'staging',
+  env: 'local',
   merchantId: 'paysafe',
   amount: 1,
   onSuccess: (...args) => {
     console.log(...args);
   },
-  wallet: {
-    publicKey: Keypair.generate().publicKey,
-    sendTransaction: () => {throw new Error('Not Implemented');},
-    signMessage: () => {throw new Error('Not Implemented');},
-  },
+  wallet,
   connection: new Connection('https://api.devnet.solana.com'),
   blockchain: 'solana',
-  color: 'black',
+  color: 'white',
+  theme: {
+    background: '#00141D',
+  },
 };
 </script>
 
 <template>
-  <coinflow-google-pay-button :args="args"/>
-  <coinflow-apple-pay-button :args="args"/>
+  <div
+    style="
+      height: 100vh;
+      width: 90vw;
+      background-color: #00141d;
+      padding-left: 40px;
+      padding-right: 40px;
+    "
+  >
+    <div style="height: 40px; padding-bottom: 40px; padding-top: 40px">
+      <coinflow-google-pay-button v-if="args" :args="args" />
+    </div>
+    <div style="height: 40px">
+      <coinflow-apple-pay-button v-if="args" :args="args" />
+    </div>
+  </div>
 </template>
